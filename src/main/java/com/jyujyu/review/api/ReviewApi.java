1e1cc2bcbead6e3c1626601d831ec9a8bc3e1919
kimdh <kdh.devs@gmail.com>
2024-02-20 23:08:52 +0900

    24/02/20 맛집에 등록된 리뷰 가져오기 API 추가

--- src/main/java/com/jyujyu/review/api/ReviewApi.java
+++ src/main/java/com/jyujyu/review/api/ReviewApi.java
@@ -2,7 +2,9 @@ package com.jyujyu.review.api;
 
 import com.jyujyu.review.api.request.CreateReviewRequest;
 import com.jyujyu.review.service.ReviewService;
+import com.jyujyu.review.service.dto.ReviewDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -20,5 +22,14 @@ public class ReviewApi {
     @DeleteMapping("/review/{reviewId}")
     public void deleteReview(@PathVariable("reviewId") Long reviewId) {
         reviewService.deleteReview(reviewId);
+    }
+
+    @GetMapping("/restaurant/{restaurantId}/reviews")
+    public ReviewDto getRestaurantReviews(
+            @PathVariable("restaurantId") Long restaurantId,
+            @RequestParam("offset") Integer offset,
+            @RequestParam("limit") Integer limit
+    ) {
+        return reviewService.getRestaurantReview(restaurantId, PageRequest.of(offset / limit, limit));
     }
 }
--- src/main/java/com/jyujyu/review/service/ReviewService.java
+++ src/main/java/com/jyujyu/review/service/ReviewService.java
@@ -3,7 +3,10 @@ package com.jyujyu.review.service;
 import com.jyujyu.review.model.ReviewEntity;
 import com.jyujyu.review.repository.RestaurantRepository;
 import com.jyujyu.review.repository.ReviewRepository;
+import com.jyujyu.review.service.dto.ReviewDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -33,6 +36,23 @@ public class ReviewService {
     public void deleteReview(Long reviewId) {
         ReviewEntity review = reviewRepository.findById(reviewId).orElseThrow();
         reviewRepository.delete(review);
+    }
+
+    public ReviewDto getRestaurantReview(Long restaurantId, Pageable page) {
+        Double avgScore = reviewRepository.getAvgScoreByRestaurantId(restaurantId);
+        Slice<ReviewEntity> reviews = reviewRepository.findSliceByRestaurantId(restaurantId, page);
+
+        return ReviewDto.builder()
+                .avgScore(avgScore)
+                .reviews(reviews.getContent())
+                .page(
+                        ReviewDto.ReviewDtoPage.builder()
+                                .offset(page.getPageNumber() * page.getPageSize())
+                                .limit(page.getPageSize())
+                                .build()
+                )
+                .build();
 
     }
+
 }
